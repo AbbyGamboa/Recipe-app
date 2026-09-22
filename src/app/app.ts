02ee76cf50a3fabb6bf4fa5e965d006091891012
Abby <agamboa@dev-10.com>
2026-09-22 11:54:17 -0500

    Have the design of card, need functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet} from '@angular/router';
 import { Header } from './header/header';
+import { RecipeCard } from './recipe-card/recipe-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, RecipeCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
