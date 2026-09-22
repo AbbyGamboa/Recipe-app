535bc6ea0613ac37973cc9ff8ca3b68514e5a03b
Abby <agamboa@dev-10.com>
2026-09-22 13:26:12 -0500

    Created a service to make http requests

--- src/app/service/recipe-service.ts
+++ src/app/service/recipe-service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Recipe } from '../recipe.type';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RecipeService {}
+export class RecipeService {
+  http = inject(HttpClient);
+  getRecipesFromApi(){
+    const url = `https://dummyjson.com/recipes`;
+    return this.http.get<Array<Recipe>>(url);
+  }
+}
